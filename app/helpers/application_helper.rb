d99d528ea59695e1afcda2da54c9579c62aa2dff
Marco Medina <mmedinalr@gmail.com>
2013-11-23 13:41:54 -0600

    Counter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def stock_counter(product)
+    {
+      on_hand: product.total_on_hand,
+      original: product.stock_items.first.stock_movements.first.quantity,
+      percentaje: (product.total_on_hand*100) / product.stock_items.first.stock_movements.first.quantity
+    }
+  end
 end
