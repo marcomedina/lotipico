e9922922ef23475772a846d8923c757adfba4f9b
Juan Carlos R <juankiz.ro@gmail.com>
2013-11-23 23:54:44 -0600

    Fix for product info page crash when there are no stock_movements.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,8 @@ module ApplicationHelper
   def stock_counter(product)
     {
       on_hand: product.total_on_hand,
-      original: product.stock_items.first.stock_movements.first.quantity,
-      percentaje: (product.total_on_hand*100) / product.stock_items.first.stock_movements.first.quantity
+      original: product.stock_items.first.stock_movements.first.try(:quantity) || 0,
+      percentaje: (product.total_on_hand*100) / (product.stock_items.first.stock_movements.first.try(:quantity) || 1)
     }
   end
 end
